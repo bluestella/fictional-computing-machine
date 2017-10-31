9133f081da6983ff0221268907d49f47841eec1b
sepharoo <iam@josephbuluran.com>
2017-10-31 13:41:03 +0800

    few edits on implem

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 import sys
 import lexer
 import re
+import os
 
+
+filetype = ".txt"
 RESERVED = 'RESERVED'
 INT      = 'INT'
 ID       = 'ID'
@@ -34,24 +37,41 @@ token_exprs = [
     (r'[A-Za-z][A-Za-z0-9_]*', ID),
 ]
 
-def main():
-    #This open input file.
-    openFile()
 
-def openFile():
-        if len(sys.argv) != 2:
-            usage()
-        filename = sys.argv[1]
-        text = open(filename).read()
-        tokens = imp_lex(text)
-        print tokens
-        for token in tokens:
-            print token
+class Interpol:
+    def main(self):
+        #Get filename of sourcefile.
+        if len(sys.argv) < 2:
+            print ("Kindly add .ipol source file.")
+            return
+        
+        src = self.__read_source_file(sys.argv[1])
+
+        if src is None or len(src) < 1:
+            print ("I don't see anything written. Write some codes. I'll interpret it for you.")
+            return
+
+        result = self.__parse_to_lexemes(src)
+        print result
+
+    def __parse_to_lexemes(self, characters):
+        return lexer.lex(characters, token_exprs)
+
+    def __read_source_file(self, filename):
+        print(chr(9))
+        if filename.endswith(filetype):
+            if(os.path.exists(filename)):
+                fileObj =  open(filename).read()
+                return fileObj
+                #return fileObj.read().splitlines()
+            else:
+                print("It seems " + filename + " does not exists!")
+        else:
+            print("Your filetype seems invalid. Interpol interpreter only accepts .ipol files.")
+         return None
 
 
-def imp_lex(characters):
-    return lexer.lex(characters, token_exprs)
+    
 
-
-if __name__ == '__main__':
-    main()
+interpol = Interpol()
+interpol.main()
